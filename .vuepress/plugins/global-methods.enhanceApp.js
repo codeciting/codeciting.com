e5bc2b94a67d41e8264b6833a8c19275122cb4db
Jagger <634750802@qq.com>
2020-04-30 17:30:45 +0800

    优化备案信息

--- .vuepress/plugins/global-methods.enhanceApp.js
+++ .vuepress/plugins/global-methods.enhanceApp.js
@@ -1,25 +1,36 @@
-export default ({ Vue, isServer }) => {
+export default ({ Vue, router }) => {
   Vue.mixin({
     methods: {
       $since (year) {
         return (new Date()).getFullYear() - year
       }
-    },
-    mounted () {
-      if (this.$el && this.$el.classList && this.$el.classList.contains('theme-container')) {
-        const footer = this.$el.getElementsByClassName('footer')[0]
-        if (footer) {
-          if (footer.getElementsByClassName('beian').length === 0) {
-            const beianEl = footer.ownerDocument.createElement('a')
-            beianEl.href = 'http://www.beian.miit.gov.cn/'
-            beianEl.target = '_blank'
-            beianEl.innerText = '辽ICP备16018554号'
-            footer.appendChild(footer.ownerDocument.createElement('br'))
-            footer.appendChild(footer.ownerDocument.createElement('br'))
-            footer.appendChild(beianEl)
-          }
-        }
-      }
     }
   })
+  const component = router.getMatchedComponents({ path: '/' })[0]
+  component.mixins.push({
+    mounted () {
+      addBeian.call(this)
+    },
+    updated () {
+      addBeian.call(this)
+    }
+  })
+}
+
+function addBeian () {
+  if (this.$el && this.$el.getElementsByClassName) {
+
+    const footer = this.$el.getElementsByClassName('footer')[0]
+    if (footer) {
+      if (footer.getElementsByClassName('beian').length === 0) {
+        const beianEl = footer.ownerDocument.createElement('a')
+        beianEl.href = 'http://www.beian.miit.gov.cn/'
+        beianEl.target = '_blank'
+        beianEl.innerText = '辽ICP备16018554号'
+        footer.appendChild(footer.ownerDocument.createElement('br'))
+        footer.appendChild(footer.ownerDocument.createElement('br'))
+        footer.appendChild(beianEl)
+      }
+    }
+  }
 }
